bd9a77939ff7c2a52e3e68911ae4da11652e20b4
Sam Serpoosh <ssjesus287@gmail.com>
2012-02-23 20:02:43 +0330

    working user signin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,26 @@ module SessionsHelper
 				current_user = user
 		end
 
+		def current_user=(user)
+				@current_user = user
+		end
+
+		def current_user
+				@current_user ||= user_from_remember_token
+		end
+
+		def signed_in?
+				!@current_user.nil?
+		end
+
+		private
+			
+			def user_from_remember_token
+					User.authenticate_with_salt(*remember_token)
+			end
+
+			def remember_token
+					cookies.signed[:remember_token] || [nil, nil]
+			end
+
 end
