8b769431462ae1dceca0cd4d0e2e072e7f58ab89
Sam Serpoosh <ssjesus287@gmail.com>
2012-02-25 20:43:57 +0330

    handled signing in and out completely with end to end tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,10 +2,6 @@ module SessionsHelper
 
 		def sign_in(user)
 				cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-				current_user = user
-		end
-
-		def current_user=(user)
 				@current_user = user
 		end
 
@@ -13,8 +9,13 @@ module SessionsHelper
 				@current_user ||= user_from_remember_token
 		end
 
+		def sign_out
+				cookies.delete(:remember_token)
+				@current_user = nil
+		end
+
 		def signed_in?
-				!@current_user.nil?
+				!current_user.nil?
 		end
 
 		private
